d2f14d75c96b288c145bd4de1bf503484cd4e743
javierhersan <javierhersan1999@gmail.com>
2023-02-02 13:45:29 +0100

    v0.0.6

--- renes_start.sh
+++ renes_start.sh
@@ -94,22 +94,22 @@ $CPE_EXEC /usr/bin/vnx_config_nat brint net1
 
 ## 7. En VNF:cpe activar arpwatch
 echo "## 7. En VNF:cpe activar arpwatch"
-# $CPE_EXEC /etc/init.d/arpwatch start
+$CPE_EXEC /etc/init.d/arpwatch start
 # $CPE_EXEC /etc/init.d/arpwatch stop
 
 ## 8. En VNF:acc activar ryu
-echo "## 8. En VNF:acc activar ryu"
-$ACC_EXEC ovs-vsctl set bridge brint protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13
-$ACC_EXEC ovs-vsctl set-fail-mode brint secure
-$ACC_EXEC ovs-vsctl set bridge brint other-config:datapath-id=0000000000000001
-$ACC_EXEC ovs-vsctl set-manager ptcp:6632
-$ACC_EXEC ovs-vsctl set-controller brint tcp:127.0.0.1:6633
-$ACC_EXEC ryu-manager ryu.app.rest_qos ryu.app.rest_conf_switch ./ryu/qos_simple_switch_13.py
+#echo "## 8. En VNF:acc activar ryu"
+#$ACC_EXEC ovs-vsctl set bridge brint protocols=OpenFlow10,OpenFlow11,OpenFlow12,OpenFlow13
+#$ACC_EXEC ovs-vsctl set-fail-mode brint secure
+#$ACC_EXEC ovs-vsctl set bridge brint other-config:datapath-id=0000000000000001
+#$ACC_EXEC ovs-vsctl set-manager ptcp:6632
+#$ACC_EXEC ovs-vsctl set-controller brint tcp:127.0.0.1:6633
+#$ACC_EXEC ryu-manager ryu.app.rest_qos ryu.app.rest_conf_switch ./ryu/qos_simple_switch_13.py
 # Manager 
-$ACC_EXEC curl -X PUT -d '"tcp:127.0.0.1:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
+#$ACC_EXEC curl -X PUT -d '"tcp:127.0.0.1:6632"' http://localhost:8080/v1.0/conf/switches/0000000000000001/ovsdb_addr
 # Cola 0: hX1 | min 8 Mbps
 # Cola 1: hX2 | max 4 Mbps
-$ACC_EXEC curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"min_rate": "8000000"}, {"max_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
+#$ACC_EXEC curl -X POST -d '{"port_name": "vxlanacc", "type": "linux-htb", "max_rate": "12000000", "queues": [{"min_rate": "8000000"}, {"max_rate": "4000000"}]}' http://localhost:8080/qos/queue/0000000000000001
 # Definir a que cola pertenece cada tráfico
-$ACC_EXEC curl -X POST -d '{"match": {"nw_dst": "'$hx1'"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
-$ACC_EXEC curl -X POST -d '{"match": {"nw_dst": "'$hx2'"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
+#$ACC_EXEC curl -X POST -d '{"match": {"nw_dst": "'$hx1'"}, "actions":{"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001
+#$ACC_EXEC curl -X POST -d '{"match": {"nw_dst": "'$hx2'"}, "actions":{"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001
